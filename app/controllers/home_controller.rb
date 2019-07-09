49593942b64465056a3c2865654ebd387713c3ce
Ryoichi iba <ryoichiaz18@gmail.com>
2019-07-09 23:45:25 +0900

    Until lesson27

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
+    if params[:ticker] == ""
+        @nothing = "Hey! You forgot to enter a symbol!"
+    elsif params[:ticker]
+        @stock = StockQuote::Stock.quote(params[:ticker])
+        if !@stock
+          @error = "HEY! That stock symbol doesnt exist!"
+        end
+    end
+  end
+
+  def about
+
   end
 end
